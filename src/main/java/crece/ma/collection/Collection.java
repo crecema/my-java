b041079bf8993a725adac74b5ff5567c6d3380d4
crecema <crece.ma@gmail.com>
2022-12-08 23:08:38 +0800

    List

---
+++ src/main/java/crece/ma/collection/Collection.java
@@ -0,0 +1,27 @@
+package crece.ma.collection;
+
+public interface Collection<E> extends Iterable<E> {
+
+    int size();
+
+    default boolean isEmpty() {
+        return size() == 0;
+    }
+
+    void add(E element);
+
+    void addAll(Collection<? extends E> elements);
+
+    void remove(E element);
+
+    void removeAll(Collection<? extends E> elements);
+
+    boolean contains(E element);
+
+    boolean containsAll(Collection<? extends E> elements);
+
+    E[] toArray();
+
+    void clear();
+
+}
