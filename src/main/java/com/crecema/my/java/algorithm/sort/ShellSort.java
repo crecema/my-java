8fd9c9727ef7740b0c29b1f469e0d2f90fe0c0d8
crecema <crece.ma@gmail.com>
2023-04-16 10:04:57 +0800

    new sort

---
+++ src/main/java/com/crecema/my/java/algorithm/sort/ShellSort.java
@@ -0,0 +1,19 @@
+package com.crecema.my.java.algorithm.sort;
+
+public class ShellSort implements Sort {
+    @Override
+    public void sort(int[] array) {
+        int h = 1;
+        while (h < array.length / 3) {
+            h = 3 * h + 1;
+        }
+        while (h >= 1) {
+            for (int i = h; i < array.length; i++) {
+                for (int j = i; j >= h && array[j] < array[j - h]; j -= h) {
+                    swap(array, j, j - h);
+                }
+            }
+            h = h / 3;
+        }
+    }
+}
