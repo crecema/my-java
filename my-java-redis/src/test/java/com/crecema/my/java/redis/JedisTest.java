034e2819ce06cc661869eb33c2572c62176d90c9
crecema <crece.ma@gmail.com>
2023-06-04 00:25:28 +0800

    feat(global): redis test

---
+++ my-java-redis/src/test/java/com/crecema/my/java/redis/JedisTest.java
@@ -0,0 +1,58 @@
+package com.crecema.my.java.redis;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import redis.clients.jedis.Jedis;
+
+public class JedisTest {
+
+    private static Jedis jedis;
+
+    @BeforeAll
+    public static void init() {
+        jedis = new Jedis("localhost", 6379);
+    }
+
+    @Test
+    void testString() {
+        System.out.println(jedis.keys("*"));
+        jedis.set("hello", "world");
+        System.out.println(jedis.keys("*"));
+        jedis.set("hello", "world2");
+        System.out.println(jedis.keys("*"));
+        jedis.expire("hello", 60);
+    }
+
+    @Test
+    void testList() {
+        jedis.lpush("list", "1");
+        jedis.lpush("list", "2");
+        jedis.lpush("list", "3");
+        System.out.println(jedis.lrange("list", 0, -1));
+    }
+
+    @Test
+    void testSet() {
+        jedis.sadd("set", "1");
+        jedis.sadd("set", "2");
+        jedis.sadd("set", "3");
+        System.out.println(jedis.smembers("set"));
+    }
+
+    @Test
+    void testZSet() {
+        jedis.zadd("zset", 1, "1");
+        jedis.zadd("zset", 2, "2");
+        jedis.zadd("zset", 3, "3");
+        System.out.println(jedis.zrange("zset", 0, -1));
+    }
+
+    @Test
+    void testHash() {
+        jedis.hset("hash", "1", "1");
+        jedis.hset("hash", "2", "2");
+        jedis.hset("hash", "3", "3");
+        System.out.println(jedis.hgetAll("hash"));
+    }
+
+}
